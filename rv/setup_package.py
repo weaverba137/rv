6ea25733a4a096f8841e8e7b13b85c62fe184ffc
Benjamin Alan Weaver <benjamin.weaver@nyu.edu>
2016-02-03 13:56:23 -0500

    adding more infrastructure

---
+++ rv/setup_package.py
@@ -0,0 +1,7 @@
+def get_package_data():
+    return {
+        _ASTROPY_PACKAGE_NAME_ + '.templates': ['*.html'],
+        _ASTROPY_PACKAGE_NAME_ + '.static.css': ['*.css'],
+        _ASTROPY_PACKAGE_NAME_ + '.static.fonts': ['glyphicons*'],
+        _ASTROPY_PACKAGE_NAME_ + '.static.js': ['*.js'],
+        }
---
+++ rv/tests/test_rv.py
@@ -0,0 +1,24 @@
+# Licensed under a 3-clause BSD style license - see LICENSE.rst
+# -*- coding: utf-8 -*-
+import re
+from .. import __version__ as rv_version
+
+
+class TestRV(object):
+    """Test the top-level rv functions.
+    """
+
+    def setup(self):
+        self.versionre = re.compile(r'''([0-9]+!)?  # epoch
+                                        ([0-9]+)    # major
+                                        (\.[0-9]+)* # minor
+                                        ((a|b|rc|\.post|\.dev)[0-9]+)?''',
+                                        re.X)
+
+    def teardown(self):
+        pass
+
+    def test_version(self):
+        """Ensure the version conforms to PEP386/PEP440.
+        """
+        assert self.versionre.match(rv_version) is not None
